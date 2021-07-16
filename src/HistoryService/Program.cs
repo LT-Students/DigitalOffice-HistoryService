50188748f1a39e20c4b708d47a76909fe111cd44
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-07-16 19:59:32 +0300

    Release/alpha 0.0.1 (#3)
    * empty service
    
    * initial (#1)
    
    * init (#2)
    
    Co-authored-by: Spartak Ryabtsev <spartak.r@gmail.com>
    Co-authored-by: Igor Likhachev <Fdiii@mail.ru>

---
+++ src/HistoryService/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+namespace LT.DigitalOffice.HistoryService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+#if DEBUG
+                .AddJsonFile("appsettings.Development.json")
+#else
+                .AddJsonFile("appsettings.Production.json")
+#endif
+                .Build();
+
+            Log.Logger = new LoggerConfiguration().ReadFrom
+                .Configuration(configuration)
+                .Enrich.WithProperty("Service", "HistoryService")
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exc)
+            {
+                Log.Fatal(exc, "Can not properly start HistoryService.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
