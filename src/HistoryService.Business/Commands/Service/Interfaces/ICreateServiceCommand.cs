a360a85dc9cb4edb08054a514b14948eabcfe679
Misha <vasiljev.mish90@gmail.com>
2021-08-30 14:48:11 +0300

    init

---
+++ src/HistoryService.Business/Commands/Service/Interfaces/ICreateServiceCommand.cs
@@ -0,0 +1,12 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.Responses;
+using System;
+
+namespace LT.DigitalOffice.HistoryService.Business.Commands.Service.Interfaces
+{
+    [AutoInject]
+    public interface ICreateServiceCommand
+    {
+        OperationResultResponse<Guid> Execute(CreateServiceRequest request);
+    }
+}
---
+++ src/HistoryService/Controllers/ServiceController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.HistoryService.Controllers
+{
+    public class ServiceController
+    {
+    }
+}
