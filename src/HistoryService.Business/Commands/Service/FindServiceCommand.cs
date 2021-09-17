65f7d1933e759efc97641878b99f94c41589deaa
Misha <vasiljev.mish90@gmail.com>
2021-09-17 14:37:48 +0300

    aad access rights to find command

--- src/HistoryService.Business/Commands/Service/FindServiceCommand.cs
+++ src/HistoryService.Business/Commands/Service/FindServiceCommand.cs
@@ -1,8 +1,9 @@
 ﻿using LT.DigitalOffice.HistoryService.Business.Commands.Service.Interfaces;
 using LT.DigitalOffice.HistoryService.Data.Interfaces;
 using LT.DigitalOffice.HistoryService.Mappers.Responses.Interfaces;
-using LT.DigitalOffice.HistoryService.Models.Db;
 using LT.DigitalOffice.HistoryService.Models.Dto.Responses;
+using LT.DigitalOffice.Kernel.AccessValidatorEngine.Interfaces;
+using LT.DigitalOffice.Kernel.Constants;
 using LT.DigitalOffice.Kernel.Enums;
 using LT.DigitalOffice.Kernel.Responses;
 using Microsoft.AspNetCore.Http;
@@ -17,19 +18,33 @@ namespace LT.DigitalOffice.HistoryService.Business.Commands.Service
         private readonly IServiceRepository _repository;
         private readonly IFindResultResponseMapper _mapper;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAccessValidator _accessValidator;
 
         public FindServiceCommand(
             IServiceRepository repository,
             IFindResultResponseMapper mapper,
+            IAccessValidator accessValidator,
             IHttpContextAccessor httpContextAccessor)
         {
             _repository = repository;
             _mapper = mapper;
             _httpContextAccessor = httpContextAccessor;
+           _accessValidator = accessValidator;
         }
 
         public OperationResultResponse<List<FindResultResponse>> Execute()
         {
+
+            if (!(_accessValidator.HasRights(Rights.AddEditRemoveHistroies)))
+            {
+                _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+
+                return new OperationResultResponse<List<FindResultResponse>>
+                {
+                  Status = OperationResultStatusType.Failed,
+                  Errors = new() { "Not enough rights." }
+                };
+            }
             OperationResultResponse<List<FindResultResponse>> response = new();
 
             response.Body = _repository.Find().Select(x => _mapper.Map(x)).ToList();
