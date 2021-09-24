8acba3fbea28b8fff3ca378e55d4de971c43e6fb
Misha <vasiljev.mish90@gmail.com>
2021-09-24 22:11:18 +0300

    init

---
+++ src/HistoryService.Models.Dto/Requests/EditServiceRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LT.DigitalOffice.HistoryService.Models.Dto.Requests
+{
+  public class EditServiceRequest
+  {
+    public string Name { get; set; }
+    public Guid ModifiedBy { get; set; }
+    public DateTime ModifiedAtUtc { get; set; }
+  }
+}
