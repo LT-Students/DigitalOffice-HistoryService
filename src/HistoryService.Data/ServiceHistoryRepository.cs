0e8836cc57b9d9a4c926fec2e42d36852ce205f1
Misha <vasiljev.mish90@gmail.com>
2021-09-27 15:58:35 +0300

    fix tabs

--- src/HistoryService.Data/ServiceHistoryRepository.cs
+++ src/HistoryService.Data/ServiceHistoryRepository.cs
@@ -9,49 +9,49 @@ using System.Linq;
 namespace LT.DigitalOffice.HistoryService.Data
 {
   public class ServiceHistoryRepository : IServiceHistoryRepository
+  {
+    private readonly IDataProvider _provider;
+
+    public ServiceHistoryRepository(
+      IDataProvider provider)
     {
-        private readonly IDataProvider _provider;
-
-        public ServiceHistoryRepository(
-            IDataProvider provider)
-        {
-            _provider = provider;
-        }
-
-        public bool DoesServiceHistoryVersionExist(string version, Guid id)
-        {
-           return _provider.ServicesHistories.Any(sh => id == sh.ServiceId && sh.Version.Contains(version));
-        }
-
-        public Guid Create(DbServiceHistory dbServiceHistory)
-        {
-            if (dbServiceHistory == null)
-            {
-                return Guid.Empty;
-            }
-
-            _provider.ServicesHistories.Add(dbServiceHistory);
-            _provider.Save();
-
-            return dbServiceHistory.Id;
-        }
-
-        public IEnumerable<DbServiceHistory> Find(FindServicesHistoriesFilter filter, out int totalCount)
-        {
-            var dbServicesHistories = _provider.ServicesHistories.AsQueryable();
-
-            if (filter.ServiceId.HasValue)
-            {
-                dbServicesHistories = dbServicesHistories.Where(sh => sh.ServiceId == filter.ServiceId.Value);
-            }
-            if (!string.IsNullOrEmpty(filter.Verison))
-            {
-                dbServicesHistories = dbServicesHistories.Where(sh => sh.Version == filter.Verison);
-            }
-
-            totalCount = dbServicesHistories.Count();
-
-            return dbServicesHistories.Skip(filter.skipCount).Take(filter.takeCount).ToList();
-        }
+      _provider = provider;
     }
+
+    public bool DoesServiceHistoryVersionExist(string version, Guid id)
+    {
+      return _provider.ServicesHistories.Any(sh => id == sh.ServiceId && sh.Version.Contains(version));
+    }
+
+    public Guid Create(DbServiceHistory dbServiceHistory)
+    {
+      if (dbServiceHistory == null)
+      {
+        return Guid.Empty;
+      }
+
+      _provider.ServicesHistories.Add(dbServiceHistory);
+      _provider.Save();
+
+      return dbServiceHistory.Id;
+    }
+
+    public IEnumerable<DbServiceHistory> Find(FindServicesHistoriesFilter filter, out int totalCount)
+    {
+      var dbServicesHistories = _provider.ServicesHistories.AsQueryable();
+
+      if (filter.ServiceId.HasValue)
+      {
+        dbServicesHistories = dbServicesHistories.Where(sh => sh.ServiceId == filter.ServiceId.Value);
+      }
+      if (!string.IsNullOrEmpty(filter.Verison))
+      {
+        dbServicesHistories = dbServicesHistories.Where(sh => sh.Version == filter.Verison);
+      }
+
+      totalCount = dbServicesHistories.Count();
+
+      return dbServicesHistories.Skip(filter.skipCount).Take(filter.takeCount).ToList();
+    }
+  }
 }
--- src/HistoryService.Models.Db/DbServiceHistory.cs
+++ src/HistoryService.Models.Db/DbServiceHistory.cs
@@ -4,42 +4,42 @@ using System;
 
 namespace LT.DigitalOffice.HistoryService.Models.Db
 {
-    public class DbServiceHistory
+  public class DbServiceHistory
+  {
+    public const string TableName = "ServicesHistories";
+
+    public Guid Id { get; set; }
+    public Guid ServiceId { get; set; }
+    public string Version { get; set; }
+    public string Content { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public Guid? ModifiedBy { get; set; }
+    public DateTime? ModifiedAtUtc { get; set; }
+
+    public DbService Service { get; set; }
+  }
+  public class DbServiceHistoryConfiguration : IEntityTypeConfiguration<DbServiceHistory>
+  {
+    public void Configure(EntityTypeBuilder<DbServiceHistory> builder)
     {
-        public const string TableName = "ServicesHistories";
+      builder.
+          ToTable(DbServiceHistory.TableName);
 
-        public Guid Id { get; set; }
-        public Guid ServiceId { get; set; }
-        public string Version { get; set; }
-        public string Content { get; set; }
-        public Guid CreatedBy { get; set; }
-        public DateTime CreatedAtUtc { get; set; }
-        public Guid? ModifiedBy { get; set; }
-        public DateTime? ModifiedAtUtc { get; set; }
+      builder.
+          HasKey(p => p.Id);
 
-        public DbService Service { get; set; }
+      builder
+          .Property(p => p.Version)
+          .IsRequired();
+
+      builder
+          .Property(p => p.Content)
+          .IsRequired();
+
+      builder
+          .HasOne(sh => sh.Service)
+          .WithMany(s => s.ServicesHistories);
     }
-    public class DbServiceHistoryConfiguration : IEntityTypeConfiguration<DbServiceHistory>
-    {
-        public void Configure(EntityTypeBuilder<DbServiceHistory> builder)
-        {
-            builder.
-                ToTable(DbServiceHistory.TableName);
-
-            builder.
-                HasKey(p => p.Id);
-
-            builder
-                .Property(p => p.Version)
-                .IsRequired();
-
-            builder
-                .Property(p => p.Content)
-                .IsRequired();
-
-            builder
-                .HasOne(sh => sh.Service)
-                .WithMany(s => s.ServicesHistories);
-        }
-    }
+  }
 }
